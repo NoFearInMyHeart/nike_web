2b6b2730895effda24f53bd2e1350f63c7b73357
zYz <zhuyueze>
2023-10-01 14:25:13 +0800

    init

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Nav from "./Nav.jsx";
+import Hero from "./Hero.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import Subscribe from "./Subscribe.jsx";
+import Footer from "./Footer.jsx";
+import ShoeCard from "./ShoeCard.jsx";
+import Button from "./Button.jsx";
+import PopularProductCard from "./PopularProductCard.jsx";
+import ServiceCard from "./ServiceCard.jsx";
+import ReviewCard from "./ReviewCard.jsx";
+
+export {
+    Nav,
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    ShoeCard,
+    Button,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
